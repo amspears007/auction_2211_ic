ed485816e265b5e9d44fb7fcb9da507167d6e4b2
Amy Spears <amspears007@gmail.com>
2023-01-08 15:05:52 -0700

    Adds 1st passing test for Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,10 +1,11 @@
 require './lib/item'
 
-RSpec.describe. do
+RSpec.describe Item do
   it 'exists and has a name' do
     item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
 
     expect(item1).to be_a(Item)
-
+    expect(item1.name).to eq('Chalkware Piggy Bank')
   end
 end
